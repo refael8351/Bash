05cf870ff261502430f3085aa3217a4dc6f4f4f3
refael8351 <wwww8351@gmail.com>
2022-12-05 16:29:07 +0200

    Update deploy_ec2.py
    add specified id  ami  Ubuntu or Debian OS

--- aws/deploy_ec2.py
+++ aws/deploy_ec2.py
@@ -2,6 +2,8 @@ import boto3
 
 
 def create_instance():
+    print(
+        "\n\t\tCopy ID Image You Want\nUbuntu 18.04 >> ami-0a59f0e26c55590e9\nUbuntu 20.04 >> ami-0d5bf08bc8017c83b\n Ubuntu 22.04 >> ami-097a2df4ac947655f\nDebian 11 >> ami-0c7c4e3c6b4941f0f\n")
     ec2 = boto3.resource('ec2')
     instances = ec2.create_instances(
         ImageId=input("\nEnter Image ID: "),

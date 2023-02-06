6dffd0fb24614eeb78f99e87f186fa3e66df44a5
Refael Malka <wwww8351@gmail.com>
2023-02-06 03:13:32 +0200

    adding more option to describe instances

--- aws/Describe_ec2.py
+++ aws/Describe_ec2.py
@@ -6,8 +6,14 @@ def describe_instance():
     response = client.describe_instances()
     for r in response['Reservations']:
         for i in r['Instances']:
-            print("\nID: " + i['InstanceId'] + "\nPrivate IP Address: " + i[
-                'PublicIpAddress'] + "\n-----------------------------\n")
+            print("\nID: " + i['InstanceId'] + "\nPrivate IP Address: " + i['PublicIpAddress'] + "\n-----------------------------\n")
 
 
 describe_instance()
+
+def describe_in():
+    client = boto3.client('ec2')
+    response = client.describe_instances()
+    for y in response['Reservations']:
+        for x in y['Instances']:
+            print(x)

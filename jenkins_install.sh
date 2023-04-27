85fdcd003ef757749e489716a3ebdd2892bd85d0
Refael Malka <wwww8351@gmail.com>
2023-04-27 16:24:02 +0300

    Create jenkins_install.sh
    Adding new File For install Jenkins

---
+++ jenkins_install.sh
@@ -0,0 +1,17 @@
+# Tested on Ubuntu Version  18.04 / 20.04
+# Must Have Root Privileges
+
+#!/bin/bash
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update -y
+sudo apt-get install fontconfig openjdk-11-jre -y
+sudo apt-get install jenkins -y
+java --version
+jenkins --version
+

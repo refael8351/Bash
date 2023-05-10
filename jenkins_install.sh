5552bd8a34df07f0eed57f31ff84fefbd3dd0dac
Refael Malka <wwww8351@gmail.com>
2023-05-10 05:33:45 +0300

    Update jenkins_install.sh
    Adding UFW Port access To 80

--- jenkins_install.sh
+++ jenkins_install.sh
@@ -1,17 +1,22 @@
-# Tested on Ubuntu Version  18.04 / 20.04
+#!/bin/bash
+
+# Ubuntu Version 18.04, 20.04, 22.04
 # Must Have Root Privileges
 
-#!/bin/bash
 
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
     /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
     https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
     /etc/apt/sources.list.d/jenkins.list > /dev/null
-
-sudo apt-get update -y
-sudo apt-get install fontconfig openjdk-11-jre -y
+sudo apt-get update -y 2>/dev/null
+sudo apt-get install ufw -y 2>/dev/null
+sudo ufw allow 80 2>/dev/null
+sudo apt-get install fontconfig openjdk-11-jre -y 2>/dev/null
 sudo apt-get install jenkins -y
-java --version
-jenkins --version
-
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+echo -e "\n\nYour Password is:\n-------------------------------\n "
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+sleep 3
+echo -e "---------------------------------\n\n\nDone!"

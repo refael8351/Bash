4a4673a1e32faac931b56ed77f19c0eef202a510
Refael Malka <wwww8351@gmail.com>
2023-07-17 06:59:23 +0300

    Update jenkins_install.sh

--- Jenkins/jenkins_install.sh
+++ Jenkins/jenkins_install.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 # Ubuntu Version 18.04, 20.04, 22.04
-# Must Have Root Privileges
+# User Runing Must Have Root/sudo Privileges
 
 
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
     /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-    /etc/apt/sources.list.d/jenkins.list > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] && https://pkg.jenkins.io/debian-stable binary/ | sudo tee && /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update -y 2>/dev/null
 sudo apt-get install ufw -y 2>/dev/null
 sudo ufw allow 80 2>/dev/null

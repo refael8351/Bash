421558748d200932cae591a2f47ebb3f30897e9c
Refael Malka <wwww8351@gmail.com>
2023-07-19 07:17:57 +0300

    Create nagios_installation.sh

---
+++ nagios_installation.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Installation of Nagios
+
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y autoconf gcc libc6 make wget unzip apache2 php libapache2-mod-php7.2 libgd-dev -y
+cd /tmp
+wget -O nagioscore.tar.gz https://github.com/NagiosEnterprises/nagioscore/archive/nagios-4.4.5.tar.gz
+tar xzf nagioscore.tar.gz
+cd /tmp/nagioscore-nagios-4.4.5/
+sudo ./configure --with-httpd-conf=/etc/apache2/sites-enabled
+sudo make all
+sudo useradd nagios
+sudo usermod -a -G nagios www-data
+sudo make install
+sudo make install-daemoninit
+sudo make install-commandmode
+sudo make install-config
+sudo make install-webconf
+sudo a2enmod rewrite
+sudo a2enmod cgi
+sudo ufw allow 'Apache'
+sudo htpasswd -c /usr/local/nagios/etc/htpasswd.users nagiosadmin
+sudo systemctl restart apache2.service
+sudo systemctl status apache2.service
+sudo systemctl start nagios.service
+sudo systemctl status nagios.service
+
+
+# Install Plugin's on Nagios
+
+cd /tmp
+wget --no-check-certificate -O nagios-plugins.tar.gz https://github.com/nagios-plugins/nagios-plugins/archive/release-2.2.1.tar.gz
+tar zxf nagios-plugins.tar.gz
+cd /tmp/nagios-plugins-release-2.2.1/
+sudo ./tools/setup
+sudo ./configure
+sudo make
+sudo make install
+
+# Restart After Installation of Plugin's
+
+sudo systemctl restart apache2.service
+sudo systemctl status apache2.service
+sudo systemctl start nagios.service
+sudo systemctl status nagios.service
+

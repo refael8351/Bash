edc29a203e84bffe8745e7366f53a0b42ea7467d
Refael Malka <wwww8351@gmail.com>
2023-08-08 04:49:27 +0300

    Update install_Docker.sh

--- Docker/install_Docker.sh
+++ Docker/install_Docker.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-installation(){
+build=`docker --version | awk '{print $1}'`
+if [ $build == "docker" ] 2>/dev/null
+then
+        echo "docker already install"
+else
         directory=`pwd`
         echo -e "\nInstall Docker on the system\n"
         sudo apt-get update -y
@@ -10,14 +14,4 @@ installation(){
         sleep 3
         bash $directory/Docker.sh
         echo -e "\nDone.."
-}
-
-
-build=`docker --version | awk '{print $1}'`
-echo "$build"
-if [ $build == "docker" ]
-then
-        echo "docker already install"
-else
-        intallation()
 fi

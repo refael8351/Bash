315bae1de2a19811185be0219c3833a2b57e756f
Refael Malka <wwww8351@gmail.com>
2023-08-13 04:54:37 +0300

    Create network_tools_install.sh

---
+++ Linux_Basic_Labs/network_tools_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Installation of Recommended Networking-Tools
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install curl -y
+sudo apt-get install wget -y
+sudo apt-get install net-tools -y
+sudo apt-get install nmap -y

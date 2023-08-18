0f9ee34a9e781f60f9ff65a3467ea55c5b7c3f52
Refael Malka <wwww8351@gmail.com>
2023-08-18 04:38:11 +0300

    Update Check_adduser.sh

--- Linux_Basic_Labs/Check_adduser.sh
+++ Linux_Basic_Labs/Check_adduser.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
+# Creating New User With No Sudo Previleges
+# Double Checking on User-Name if it's Exist and Check if Password`s is Equal
 
-group="sudo"
-read -p> "Set new sudo user's name (Please try to avoid admin or administrator, some hosts have these reserved): " new_sudo_user_name
-if [ -z "$(getent passwd $new_sudo_user_name)" ]; then
-        echo "user does NOT exist."
+read -p "Set new user..... Enter a Name: (Please Try To Avoid admin or administrator): " user_name
+if [ -z "$(getent passwd $user_name)" ]
+then
+        echo "user DOES NOT exist."
+        echo "Please enter the new password:"
+        read password1
+        echo "Please repeat the new password:"
+        read password2
+        if [ $password1 != $password2 ]
+        then
+                echo "\nPasswords do not match\n"
+                exit
+                echo "\nCancel Useradd Proccess"
+                sleep 2
+                echo "\nDone..\nStatus: Failed !!"
+        fi
+        sudo useradd -d /home/$user_name --create-home -p $password1 $user_name
+        id $user_name
+        cd /home/$user_name
 else
-        echo "user DOES exist."
+        echo "\nThe User-Name Already in Used\nDone..\nStatus: Failed !!"
 fi
 
-echo "Please enter the new password:"
-read password1
-echo "Please repeat the new password:"
-read password2
-
-if [ $password1 != $password2 ]; then
-        echo "Passwords do not match"
-        exit
-fi
-
-sudo su -c "useradd $new_sudo_user_name -s /bin/bash -m -aG $group"

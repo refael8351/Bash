646e6690aff9e995b8b08660cddbdb3c50562c73
Refael Malka <wwww8351@gmail.com>
2023-08-19 07:55:40 +0300

    Update CD_part3.sh

--- Continues_Delivery/CD_part3.sh
+++ Continues_Delivery/CD_part3.sh
@@ -5,19 +5,22 @@
 # Deploy 3 Container + IP Address of Each once
 
 # Container 1
-sudo docker run -itd --name my1 nginx /bin/bash -c 'for i in {1..1440} ; do `hostname -I` ; echo $i ; sleep 1 ; done'
+read -p "Name of Contaier 1: " con1
+sudo docker run -itd --name $con1 nginx /bin/bash -c 'for i in {1..1440} ; do `hostname -I` ; echo $i ; sleep 1 ; done'
 sleep 3
 echo -e "\n\nIP Address:"
-sudo docker logs my1 | head | awk 'NR==1 {print $4}'
+sudo docker logs $con1 | head | awk 'NR==1 {print $4}'
 
 # Container 2
-sudo docker run -itd --name my2 nginx /bin/bash -c 'for i in {1..1440} ; do `hostname -I` ; echo $i ; sleep 1 ; done'
+read -p "Name of Contaier 2: " con2
+sudo docker run -itd --name $con2 nginx /bin/bash -c 'for i in {1..1440} ; do `hostname -I` ; echo $i ; sleep 1 ; done'
 sleep 3
 echo -e "\n\nIP Address:"
-sudo docker logs my2 | head | awk 'NR==1 {print $4}'
+sudo docker logs $con2 | head | awk 'NR==1 {print $4}'
 
 # Container 3
-sudo docker run -itd --name my3 nginx /bin/bash -c 'for i in {1..1440} ; do `hostname -I` ; echo $i ; sleep 1 ; done'
+read -p "Name of Contaier 3: " con3
+sudo docker run -itd --name $con3 nginx /bin/bash -c 'for i in {1..1440} ; do `hostname -I` ; echo $i ; sleep 1 ; done'
 sleep 3
 echo -e "\n\nIP Address:"
-sudo docker logs my3 | head | awk 'NR==1 {print $4}'
+sudo docker logs $con3 | head | awk 'NR==1 {print $4}'

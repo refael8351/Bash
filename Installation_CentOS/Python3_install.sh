7a46acfa90310e8031862fa30075cef10a3fbc4f
Refael Malka <wwww8351@gmail.com>
2023-08-19 20:39:45 +0300

    Create Python3_install.sh

---
+++ Installation_CentOS/Python3_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Apply for CentOS/RedHat Distribution
+
+echo -e "\n\ninstallation of Python3 + pip Start...\n\n"
+sudo yum update -y
+sudo yum install python3 -y
+sudo yum install python3-pip -y

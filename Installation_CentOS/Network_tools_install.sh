e987cc01806960562be47baaee0cffc02328eed0
Refael Malka <wwww8351@gmail.com>
2023-08-19 20:39:42 +0300

    Create Network_tools_install.sh

---
+++ Installation_CentOS/Network_tools_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Apply for CentOS/RedHat Distribution
+
+sudo yum update -y
+sudo yum upgrade -y
+sudo yum install curl -y
+sudo yum install wget -y
+sudo yum install net-tools -y
+sudo yum install nmap -y

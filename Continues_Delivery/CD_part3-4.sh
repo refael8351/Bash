867c9ea1f4a84955ad01bd0310ce03cd167425c2
Refael Malka <wwww8351@gmail.com>
2023-08-19 08:09:29 +0300

    Rename File

--- Continues_Delivery/CD_part3-4.sh
+++ Continues_Delivery/CD_part3-4.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Simple CD Procces >>> Part 2
+# Simple CD Procces          >>>        Part  [ 3/4 ]
+# Simple CD Procces          >>>        Part  [ 4/4 ]
+
 
 # Deploy 3 Container (Live 24H Only) and Display IP Address of Each once
 
 # Container 1
+
 read -p "Name of Contaier 1: " con1
 sudo docker run -itd --name $con1 nginx /bin/bash -c 'for i in {1..1440} ; do `hostname -I` ; echo $i ; sleep 1 ; done'
 sleep 3
@@ -12,6 +15,7 @@ echo -e "\n\nIP Address:"
 sudo docker logs $con1 | head | awk 'NR==1 {print $4}'
 
 # Container 2
+
 read -p "Name of Contaier 2: " con2
 sudo docker run -itd --name $con2 nginx /bin/bash -c 'for i in {1..1440} ; do `hostname -I` ; echo $i ; sleep 1 ; done'
 sleep 3
@@ -19,6 +23,7 @@ echo -e "\n\nIP Address:"
 sudo docker logs $con2 | head | awk 'NR==1 {print $4}'
 
 # Container 3
+
 read -p "Name of Contaier 3: " con3
 sudo docker run -itd --name $con3 nginx /bin/bash -c 'for i in {1..1440} ; do `hostname -I` ; echo $i ; sleep 1 ; done'
 sleep 3

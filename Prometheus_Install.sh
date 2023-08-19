8a54b0f3e16cf63c66f93f951dd9269a650b4cd2
Refael Malka <wwww8351@gmail.com>
2023-08-19 05:08:18 +0300

    Update Prometheus_Install.sh

--- Prometheus_Install.sh
+++ Prometheus_Install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd
 sudo wget -nc https://github.com/prometheus/prometheus/releases/download/v2.46.0/prometheus-2.46.0.linux-amd64.tar.gz
 tar xvfz prometheus-*.tar.gz
 cd prometheus-*

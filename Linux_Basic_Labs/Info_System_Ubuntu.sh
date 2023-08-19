42208338478f7794e2b5e23878cacfb5a0b5289d
Refael Malka <wwww8351@gmail.com>
2023-08-19 15:10:44 +0300

    Update Info_System_Ubuntu.sh

--- Linux_Basic_Labs/Info_System_Ubuntu.sh
+++ Linux_Basic_Labs/Info_System_Ubuntu.sh
@@ -5,7 +5,7 @@
 
 echo "\n\n< System infomation >\n\nIP address: `hostname -I | awk '{print $2}'`"
 echo "RAM Memory: `free -h | awk 'NR==2 {print $2}'`"
-echo "Storage Used: `df -h | grep /sda1 | awk '{print $5}'`"
+echo "Storage Used: `df -h | grep /sda | awk '{print $5}'`"
 echo "Storage Total: `df -h | grep /$ | awk '{print $2}'`"
 echo "Karnel Version: `uname -r`"
 echo "Distribution OS: `cat /etc/*release | grep ID | awk 'NR==1' | cut -d "=" -f 2` `cat /etc/*release | grep VERSION | awk 'NR==1' | cut -d " " -f 2 | cut -d "=" -f2`"

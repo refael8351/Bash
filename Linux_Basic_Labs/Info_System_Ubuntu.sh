4b18302fbd9fb94416ef57d5660ca1d2444d075b
Refael Malka <wwww8351@gmail.com>
2023-08-19 14:56:37 +0300

    Update Info_System_Ubuntu.sh

--- Linux_Basic_Labs/Info_System_Ubuntu.sh
+++ Linux_Basic_Labs/Info_System_Ubuntu.sh
@@ -3,10 +3,10 @@
 # Show information about System  (( Ubuntu_18.04 / 20.04 / 22.04 ))
 
 
-echo -e "\n\n< System infomation >\n\nIP address: `hostname -I | awk '{print $2}'`"
-echo -e "RAM Memory: `free -h | awk 'NR==2 {print $2}'`"
-echo -e "Storage Used: `df -h | grep /sda1`"
-echo -e "Storage Total: `df -h | grep /$ | awk '{print $2}'`"
-echo -e "Karnel Version: `uname -r`"
-echo -e "Distribution OS: `cat /etc/*release | grep ID | awk 'NR==1' | cut -d "=" -f 2` `cat /etc/*release | grep VERSION | awk 'NR==1' | cut -d " " -f 2`"
+echo "\n\n< System infomation >\n\nIP address: `hostname -I | awk '{print $2}'`"
+echo "RAM Memory: `free -h | awk 'NR==2 {print $2}'`"
+echo "Storage Used: `df -h | grep /sda1 | awk '{print $5}'`"
+echo "Storage Total: `df -h | grep /$ | awk '{print $2}'`"
+echo "Karnel Version: `uname -r`"
+echo "Distribution OS: `cat /etc/*release | grep ID | awk 'NR==1' | cut -d "=" -f 2` `cat /etc/*release | grep VERSION | awk 'NR==1' | cut -d " " -f 2`"
 
